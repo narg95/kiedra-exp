563b9ebc15f2c4ef67745ee267f031d96ea0e461
nestor <nesterran@gmail.com>
2016-12-03 10:47:22 +0100

    Calculate accumulated TP TN

--- analyze.py
+++ analyze.py
@@ -29,20 +29,42 @@ def confusion_matrix(
     true_positives = len([variable for variable in found_variables if variable in relevant_variables])
     true_negatives = len([variable for variable in discarted_variables if variable in noisy_variables])
 
-    false_positives = len([variable for variable in found_variables if variable in noisy_variables])
-    false_negatives = len([variable for variable in discarted_variables if variable in relevant_variables])
-    
-    return [true_positives, true_negatives, false_positives, false_negatives]
+    return [true_positives, true_negatives]
 
 def run_analysis():
     results = load_results('./results.tab')
-    analysis = [ [file, noise, elapsed, len(relevant), len(noisy),
+    analysis = [ [file,
             confusion_matrix(relevant, noisy, r_scores, 0.0), 
             confusion_matrix(relevant, noisy, k_scores, 0.7)] 
-        for [file, noise, elapsed, relevant, noisy, r_scores, k_scores] in results
+        for [file, _, _, relevant, noisy, r_scores, k_scores] in results
         ]
 
-    save_to_file(analysis)
+    aggregated_result = {}
+    for [file, [r_tp, r_tn], [k_tp, k_tn]] in analysis:
+        problem_key = file.split('-')[0]
+        key = (problem_key, r_tp, r_tn)
+
+        r_acc, k_acc, acc = calculate_accumulated_values(aggregated_result, key, False)
+        aggregated_result[key] = [problem_key, r_tp, r_tn, r_acc, k_acc, acc]
+
+        key = (problem_key, k_tp, k_tn)
+        r_acc, k_acc, acc = calculate_accumulated_values(aggregated_result, key, True)
+        aggregated_result[key] = [problem_key, k_tp, k_tn, r_acc, k_acc, acc]
+    
+    save_to_file(aggregated_result.values())
+
+def calculate_accumulated_values(aggregated_result, key, is_kiedra):
+    if key not in aggregated_result:
+        r_acc = 0 if is_kiedra else 1
+        k_acc = 1 if is_kiedra else 0
+        return r_acc, k_acc, 1
+
+    [r_acc, k_acc, acc] = aggregated_result[key][3:]      
+    r_acc = r_acc if is_kiedra else r_acc + 1
+    k_acc = k_acc + 1 if is_kiedra else k_acc
+    acc = acc + 1
+    return r_acc, k_acc, acc
+
 
 def save_to_file(analysis):
     text_results = ['\t'.join([str(val) for val in data]) + '\n' for data in analysis]
